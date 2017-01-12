76ebcb2be66825d74e5e39493d822c6e2e2daf0a
crynobone <crynobone@gmail.com>
2017-01-12 22:55:06 +0800

    Update example.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- example.php
+++ example.php
@@ -2,14 +2,13 @@
 
 require "vendor/autoload.php";
 
-$promises = Laravie\Promise\EventPromises::create();
-
-$promises->then(function ($url) {
-    print("Fetching {$url}".PHP_EOL);
-    return [$url, file_get_contents($url)];
-})
-->then(function ($value) {
-    printf("Fetched %s Receiving %d bytes\n", $value[0], strlen($value[1]));
-})
-->queues(['https://google.com', 'https://www.katsana.com', 'https://kakitangan.com'])
-->all();
+Laravie\Promise\EventPromises::create()
+    ->then(function ($url) {
+        print("Fetching {$url}".PHP_EOL);
+        return [$url, file_get_contents($url)];
+    })
+    ->then(function ($value) {
+        printf("Fetched %s Receiving %d bytes\n", $value[0], strlen($value[1]));
+    })
+    ->queues(['https://google.com', 'https://www.katsana.com', 'https://kakitangan.com'])
+    ->all();
