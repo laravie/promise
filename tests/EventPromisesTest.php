91cdc7c5c86bca8ff0262b5de42ac8f4d2a48c94
crynobone <crynobone@gmail.com>
2017-08-31 10:35:05 +0800

    Prepare 1.0 alias to master branch
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ tests/EventPromisesTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use Laravie\Promise\EventPromises;
+
+class EventPromisesTest extends PHPUnit_Framework_TestCase
+{
+    /** @test */
+    public function it_can_throws_exception()
+    {
+        $promises = EventPromises::create();
+
+        $promises->then(function ($value) {
+            return $value + 1;
+        })
+        ->then(function ($value) {
+            throw new Exception($value + 1);
+        })
+        ->otherwise(function ($value) {
+            return $value->getMessage() + 1;
+        })
+        ->queues([1, 2])
+        ->all()
+        ->then(function ($values) {
+            $this->assertEquals([4, 5], $values);
+        });
+    }
+}
