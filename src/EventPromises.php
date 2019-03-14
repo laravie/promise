57939ccdaa4182c8956c9d21012e522ab120ae85
Mior Muhammad Zaki <crynobone@gmail.com>
2019-03-14 22:56:14 +0800

    Rename variables.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/EventPromises.php
+++ src/EventPromises.php
@@ -8,29 +8,36 @@ use React\EventLoop\LoopInterface;
 class EventPromises extends Actionable
 {
     /**
+     * The event-loop implementation.
+     *
+     * @var \React\EventLoop\LoopInterface
+     */
+    protected $eventLoop;
+
+    /**
      * Create a new collection.
      *
-     * @param \React\EventLoop\LoopInterface|null $loop
+     * @param \React\EventLoop\LoopInterface|null $eventLoop
      *
      * @return static
      */
-    public static function create(LoopInterface $loop = null)
+    public static function create(LoopInterface $eventLoop = null)
     {
-        return new static($loop);
+        return new static($eventLoop);
     }
 
     /**
      * Construct async promises.
      *
-     * @param \React\EventLoop\LoopInterface|null $loop
+     * @param \React\EventLoop\LoopInterface|null $eventLoop
      */
-    public function __construct(LoopInterface $loop = null)
+    public function __construct(LoopInterface $eventLoop = null)
     {
-        if (\is_null($loop)) {
-            $loop = Factory::create();
+        if (\is_null($eventLoop)) {
+            $eventLoop = Factory::create();
         }
 
-        $this->loop = $loop;
+        $this->eventLoop = $eventLoop;
     }
 
     /**
@@ -43,12 +50,12 @@ class EventPromises extends Actionable
         $promises = [];
 
         foreach ($this->promises as $data) {
-            $this->loop->futureTick(function () use ($data, &$promises) {
+            $this->eventLoop->futureTick(function () use ($data, &$promises) {
                 $promises[] = $this->buildPromise($data);
             });
         }
 
-        $this->loop->run();
+        $this->eventLoop->run();
 
         return $promises;
     }
